4c31ddc9e89d1b95511d817ff8355f6ec69644f9
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-10-12 10:07:57 +0600

    add

--- ChildCareApp/app/src/main/java/com/nusrat/childcareapp/MainActivity.java
+++ ChildCareApp/app/src/main/java/com/nusrat/childcareapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.nusrat.childcareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        ImageView splash=findViewById(R.id.splash);
+        splash.setAnimation(fadeIn);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent=new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
 }
