5ba4af5b83de91cf23dbe290389e25259d0083a2
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-10-12 18:55:24 +0600

    add

---
+++ ChildCareApp/app/src/main/java/com/nusrat/childcareapp/AdviceActivity.java
@@ -0,0 +1,34 @@
+package com.nusrat.childcareapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AdviceActivity extends AppCompatActivity {
+
+    private Button btn_vd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_advice);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_vd = findViewById(R.id.btn_vd);
+        btn_vd.setOnClickListener(v -> {
+            Intent intent = new Intent(AdviceActivity.this, VaccineDetails.class);
+            startActivity(intent);
+        });
+    }
+}
--- ChildCareApp/app/src/main/java/com/nusrat/childcareapp/Home.java
+++ ChildCareApp/app/src/main/java/com/nusrat/childcareapp/Home.java
@@ -1,7 +1,13 @@
 package com.nusrat.childcareapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
+import com.nusrat.childcareapp.R;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
+    private RadioGroup rgAgeGroup;
+    private Button btnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,43 @@ public class Home extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        rgAgeGroup = findViewById(R.id.rg_age_group);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+//            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onClick(View v) {
+                int selectedId = rgAgeGroup.getCheckedRadioButtonId();
+                Intent intent = new Intent(Home.this, AdviceActivity.class);
+
+                if (selectedId == R.id.rbage01) {
+                    intent.putExtra("ageGroup", "0-1");
+                } else if (selectedId == R.id.rbage13) {
+                    intent.putExtra("ageGroup", "1-3");
+                } else if (selectedId == R.id.rbage47) {
+                    intent.putExtra("ageGroup", "4-7");
+                } else if (selectedId == R.id.rbage810) {
+                    intent.putExtra("ageGroup", "8-10");
+                }
+//                switch (selectedId) {
+//                    case R.id.rbage01:
+//                        intent.putExtra("ageGroup", "0-1");
+//                        break;
+//                    case R.id.rbage13:
+//                        intent.putExtra("ageGroup", "1-3");
+//                        break;
+//                    case R.id.rbage47:
+//                        intent.putExtra("ageGroup", "4-7");
+//                        break;
+//                    case R.id.rbage810:
+//                        intent.putExtra("ageGroup", "8-10");
+//                        break;
+//                }
+                startActivity(intent);
+            }
         });
     }
 }
