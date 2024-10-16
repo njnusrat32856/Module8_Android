ae54b0f264d70ee5bcba67647f18ef84e53ea10d
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-10-16 16:52:14 +0600

    final

--- childcare/app/src/main/java/com/nusrat/childcare/HealthCare.java
+++ childcare/app/src/main/java/com/nusrat/childcare/HealthCare.java
@@ -27,7 +27,7 @@ public class HealthCare extends AppCompatActivity {
             return insets;
         });
 
-        // Initialize buttons
+        // buttons
         fv = findViewById(R.id.btnfev);
         cc = findViewById(R.id.btncc);
         sa = findViewById(R.id.btnsa);
@@ -42,66 +42,66 @@ public class HealthCare extends AppCompatActivity {
                 openActivityFever();
             }
         });
-//        cc.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openActivityCold();
-//            }
-//        });
-//        sa.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openActivitySA();
-//            }
-//        });
-//        ga.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openActivityGA();
-//            }
-//        });
-//        c.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openActivityConsti();
-//            }
-//        });
-//        h.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openActivityHicc();
-//            }
-//        });
+        cc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityCold();
+            }
+        });
+        sa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitySA();
+            }
+        });
+        ga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityGA();
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityConsti();
+            }
+        });
+        h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityHicc();
+            }
+        });
     }
 
-    // Methods to open different activities
+    // open different activities
     public void openActivityFever() {
         Intent intent = new Intent(this, FeverActivity.class);
         startActivity(intent);
     }
 
-//    public void openActivityCold() {
-//        Intent intent = new Intent(this, CommonCold.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openActivitySA() {
-//        Intent intent = new Intent(this, StomachAche.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openActivityGA() {
-//        Intent intent = new Intent(this, GasandAcidity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openActivityConsti() {
-//        Intent intent = new Intent(this, Constipation.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openActivityHicc() {
-//        Intent intent = new Intent(this, Hiccups.class);
-//        startActivity(intent);
-//    }
+    public void openActivityCold() {
+        Intent intent = new Intent(this, CommonCold.class);
+        startActivity(intent);
+    }
+
+    public void openActivitySA() {
+        Intent intent = new Intent(this, StomachAche.class);
+        startActivity(intent);
+    }
+
+    public void openActivityGA() {
+        Intent intent = new Intent(this, GasandAcidity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityConsti() {
+        Intent intent = new Intent(this, Constipation.class);
+        startActivity(intent);
+    }
+
+    public void openActivityHicc() {
+        Intent intent = new Intent(this, Hiccups.class);
+        startActivity(intent);
+    }
 }
